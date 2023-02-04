36e21af6d75ce399654e1db5567786cb2452bb1e
Beytullah KALAY <beytullah.kalay@outlook.com>
2023-02-04 19:05:28 +0300

    modified: enemy health system

--- Assets/Scripts/Enemy/EnemyHealthSystem.cs
+++ Assets/Scripts/Enemy/EnemyHealthSystem.cs
@@ -1,8 +1,17 @@
 using AbstractClasses;
+using Enemy.State;
+using UnityEngine.AI;
 
 namespace Enemy
 {
     public class EnemyHealthSystem : HealthSystem
     {
+        public override void Die()
+        {
+            base.Die();
+            GetComponent<EnemyAnimationController>().TriggerDeadAnim();
+            GetComponent<EnemyStateManager>().enabled = false;
+            GetComponent<NavMeshAgent>().Stop();
+        }
     }
 }
